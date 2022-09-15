0817386d168332bb34a543914ba22e2e8684ed5b
Pheubel <luca.guagliardo@live.nl>
2022-09-15 11:27:19 +0200

    minimal input processing

--- Compiler/NovelerCompiler/Program.cs
+++ Compiler/NovelerCompiler/Program.cs
@@ -1,56 +1,101 @@
 ﻿using Noveler;
 using System.CommandLine;
 using System.Runtime.InteropServices;
+using System.Text;
 
 public class Program
 {
+    const string NovelExtension = ".nvl";
+    const string CompiledNovelExtension = ".cnvl";
+
     static async Task<int> Main(string[] args)
     {
         var fileOption = new Option<FileInfo?>(
-            name: "--file",
+            name: "--input",
             description: "The file to read and display on the console.",
             parseArgument: result =>
             {
-                if (result.Tokens.Count == 0)
-                {
-                    result.ErrorMessage = "";
-                    return null;
-                }
-
                 string? filePath = result.Tokens.Single().Value;
 
                 if (!File.Exists(filePath))
                 {
-                    result.ErrorMessage = "File does not exist";
+                    result.ErrorMessage = "Error: File does not exist.";
                     return null;
                 }
 
-                return new FileInfo(filePath);
+                var fileInfo = new FileInfo(filePath);
+
+                if (fileInfo.Extension != NovelExtension)
+                    Console.WriteLine($"Warning: Input file is not a \"{NovelExtension}\" file.");
+
+                return fileInfo;
             });
 
         var outputOption = new Option<FileInfo?>(
             name: "--output",
-            description: "The file ot output the compiled script to. "
-            );
+            description: "The file to output the compiled script to.",
+            parseArgument: result =>
+            {
+                string? filePath = result.Tokens.Single().Value;
 
-        var rootCommand = new RootCommand("Compiles Noveler scripts into easy to interpret instructions forthe Noveler runtime.");
+                var fileInfo = new FileInfo(filePath);
+
+                if (fileInfo.Exists)
+                {
+                    Console.WriteLine("Warning: output file has been overwritten.");
+                }
+
+                return fileInfo;
+            });
+
+        fileOption.Arity = new ArgumentArity(1, 1);
+        outputOption.Arity = new ArgumentArity(1, 1);
+
+        fileOption.IsRequired = true;
+        outputOption.IsRequired = true;
+
+        fileOption.AddAlias("-i");
+        outputOption.AddAlias("-o");
+
+        var rootCommand = new RootCommand("Compiles Noveler scripts into easy to interpret instructions for the Noveler runtime.");
         rootCommand.AddOption(fileOption);
         rootCommand.AddOption(outputOption);
 
         rootCommand.SetHandler((inputFile, outputFile) =>
         {
-            var scriptString = File.ReadAllText(inputFile!.FullName);
+            if (inputFile == null)
+            {
+                Console.WriteLine("Missing input file. See --help");
+                return Task.FromResult(ReturnCode.MissingInput);
+            }
+
+            if (outputFile == null)
+            {
+                Console.WriteLine("Missing output destination. See --help");
+                return Task.FromResult(ReturnCode.MissingInput);
+            }
+
+            var scriptString = File.ReadAllText(inputFile.FullName);
 
             var resultCode = Compiler.Compile(scriptString, out string compileResult);
 
             if (resultCode != Compiler.Result.Succes)
-                return Task.FromResult(resultCode);
+                return Task.FromResult(ReturnCode.CompilerError);
 
-            string destination = outputFile!.FullName;
+            string destination = outputFile.Extension == CompiledNovelExtension ?
+                outputFile.FullName :
+                outputFile.FullName + CompiledNovelExtension;
 
-            using (var output = File.OpenWrite(destination))
+            try
             {
+                using var output = File.OpenWrite(destination);
+
                 output.Write(MemoryMarshal.AsBytes(compileResult.AsSpan()));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Unable to write the compile output to \"{destination}\".");
+                return Task.FromResult(ReturnCode.OutputFailure);
             }
 
             return Task.FromResult(resultCode);
@@ -58,6 +103,14 @@ public class Program
             fileOption, outputOption);
 
         return await rootCommand.InvokeAsync(args);
+    }
+
+    public enum ReturnCode
+    {
+        Succes,
+        MissingInput,
+        OutputFailure,
+        CompilerError,
     }
 }
 
@@ -67,9 +120,15 @@ namespace Noveler
     {
         public static Result Compile(string script, out string result)
         {
-            // as example reverse the input
-            result = script.Reverse().ToString()!;
+            StringBuilder stringBuilder = new StringBuilder();
 
+            // as example reverse the input
+            foreach (var c in script.Reverse())
+            {
+                stringBuilder.Append(c);
+            }
+
+            result = stringBuilder.ToString();
             return Result.Succes;
         }
 
