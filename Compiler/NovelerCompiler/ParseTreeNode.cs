96e872e7df771553c0e10986ac8bcbfa39cf782e
Pheubel <luca.guagliardo@live.nl>
2022-10-26 14:28:57 +0200

    added tree visualizer

--- Compiler/NovelerCompiler/ParseTreeNode.cs
+++ Compiler/NovelerCompiler/ParseTreeNode.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.VisualBasic;
-using Noveler.Compiler;
+﻿using Noveler.Compiler;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace NovelerCompiler
 {
@@ -22,6 +22,10 @@ namespace NovelerCompiler
 
             if (kind != NodeKind.Token)
                 Children = new List<ParseTreeNode>();
+            else
+            {
+                GrammarKind = GrammarKind.Token;
+            }
         }
 
         /// <summary>
@@ -42,6 +46,45 @@ namespace NovelerCompiler
                 case NodeKind.Sequence:
                     Children!.AddRange(node.Children!);
                     break;
+            }
+        }
+
+        public string VisualizeTree()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{GrammarKind}{(Token != null ? $": {Token.Type}" : string.Empty)}");
+
+            int childCount = Children?.Count ?? 0;
+            for (var i = 0; i < childCount; i++)
+            {
+                Children![i].VisualizeTree(sb, string.Empty, (i == (childCount - 1)));
+            }
+
+            return sb.ToString();
+        }
+
+        private void VisualizeTree(StringBuilder output, string indent, bool isLast)
+        {
+            output.Append(indent);
+
+            if (isLast)
+            {
+                output.Append("└─");
+                indent += "  ";
+            }
+            else
+            {
+                output.Append("├─");
+                indent += "│ ";
+            }
+
+            output.AppendLine($"{GrammarKind}{(Token != null ? $": {Token.Type}" : string.Empty)}");
+
+            var numberOfChildren = Children?.Count ?? 0;
+            for (var i = 0; i < numberOfChildren; i++)
+            {
+                Children![i].VisualizeTree(output, indent, (i == (numberOfChildren - 1)));
             }
         }
 
