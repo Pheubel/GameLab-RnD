2b5da3ef7549c03561bc6df6fe8f402e15f424d1
Pheubel <luca.guagliardo@live.nl>
2023-03-10 18:36:58 +0100

    added symbol visitor

---
+++ Compiler/NovelerCompiler/NovelerCompiler/NovelerSymbolVisitor.cs
@@ -0,0 +1,46 @@
+﻿using Noveler.Compiler.Grammar;
+
+namespace Noveler.Compiler
+{
+	internal sealed class NovelerSymbolVisitor : NovelerParserBaseVisitor<Dictionary<string, SymbolInfo>?>
+	{
+		public override Dictionary<string, SymbolInfo> VisitStory(NovelerParser.StoryContext context)
+		{
+			Dictionary<string, SymbolInfo> symbolTable = new(128);
+
+			var typeVisitor = new TypeVisitor(symbolTable);
+			typeVisitor.VisitStory(context);
+
+			return symbolTable;
+		}
+
+		private sealed class TypeVisitor : NovelerParserBaseVisitor<nint>
+		{
+			private readonly Dictionary<string, SymbolInfo> _symbolTable;
+
+			public TypeVisitor(Dictionary<string, SymbolInfo> symbolTable)
+			{
+				_symbolTable = symbolTable;
+			}
+
+			public override nint VisitType_name(NovelerParser.Type_nameContext context)
+			{
+				var typeName = context.GetText();
+
+				Console.WriteLine($"found type_name: {typeName}");
+				_symbolTable.TryAdd(typeName, new SymbolInfo());
+
+				return default;
+			}
+
+			public override nint VisitIdentifier(NovelerParser.IdentifierContext context)
+			{
+				var identifierName = context.GetText();
+
+				Console.WriteLine($"found type_name: {identifierName} ({context.Start.Line}:{context.Start.Column} ... {context.Stop.Line}:{context.Stop.StopIndex})");
+				_symbolTable.TryAdd(identifierName, new SymbolInfo());
+				return default;
+			}
+		}
+	}
+}
---
+++ Compiler/NovelerCompiler/NovelerCompiler/SymbolInfo.cs
@@ -0,0 +1,26 @@
+﻿using SymbolDefinition = OneOf.OneOf<OneOf.Types.Unknown>;
+
+namespace Noveler.Compiler
+{
+	internal sealed class SymbolInfo
+	{
+		public SymbolInfo()
+		{
+			Definition = new OneOf.Types.Unknown();
+		}
+
+		public SymbolDefinition Definition { get; private set; }
+		public bool IsDefined => !Definition.IsT0;
+
+		public bool SetDefinition(SymbolDefinition definition)
+		{
+			if (definition.IsT0)
+				throw new ArgumentException("Cannot set definition to unknown.", nameof(definition));
+
+			if (Definition.IsT0)
+				return false;
+
+			return true;
+		}
+	}
+}
